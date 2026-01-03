c30cbd61815ece78d68b6aa0434d82d654072eaf
sajal-jayanto <sajaljayanto007@gmail.com>
2026-01-03 22:20:51 +0600

    comment remove

--- src/lib/queue.ts
+++ src/lib/queue.ts
@@ -15,39 +15,3 @@ export const articleTaskQueue = new Queue(QueueList.ARTICLE_TASK_QUEUE, {
     removeOnComplete: true,
   },
 });
-
-// export const addTask = async (name: string, data: any) => {
-//   return await ArticleTaskQueue.add(name, data);
-// };
-
-
-// import { Worker, Job } from 'bullmq';
-// import { redisConnection } from '../lib/Queue';
-
-// export const startWorker = () => {
-//   const worker = new Worker(
-//     'MAIN_TASK_QUEUE', 
-//     async (job: Job) => {
-//       // Logic for different job types
-//       switch (job.name) {
-//         case 'sendEmail':
-//           console.log(`Sending email to: ${job.data.email}`);
-//           // await emailService.send(...)
-//           break;
-
-//         case 'processVideo':
-//           console.log(`Processing video ID: ${job.data.id}`);
-//           break;
-
-//         default:
-//           console.warn(`No processor for job: ${job.name}`);
-//       }
-//     }, 
-//     { connection: redisConnection }
-//   );
-
-//   worker.on('completed', (job) => console.log(`✅ Job ${job.id} finished`));
-//   worker.on('failed', (job, err) => console.error(`❌ Job ${job?.id} failed: ${err.message}`));
-
-//   console.log('🚀 Worker is listening for tasks...');
-// };
